f2313cc18ae2898e7407cf60b2f05ad67bf66621
Hassan Sarfraz <dkspklhrd003@gmail.com>
2026-07-22 17:12:00 +0500

    speaker fix to visible

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -343,7 +343,6 @@ export const speakers = mysqlTable("speakers", {
     seoData: json("seo_data"),
     customFields: json("custom_fields"),
     order: int("order").default(0).notNull(),
-    isActive: boolean("is_active").default(true).notNull(),
     ...timestamps,
 });
 
